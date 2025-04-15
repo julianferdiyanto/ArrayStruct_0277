cb1c4135c47eab41051e9d22a2a395f1102856b4
julianferdiyanto <julianferdiyanto3@gmail.com>
2025-04-15 11:46:44 +0700

    menambahkan fungsi int main pada program struct

--- struct.cpp
+++ struct.cpp
@@ -7,3 +7,25 @@ struct mahasiswa
     string nama;
     string alamat;
 };
+
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim = "20240140277";
+    mhs.nama = "Julian Ferdiyanto";
+    mhs.alamat = "Kasihan";
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan NAMA = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat = ";
+    cin >> mhs.alamat;
+
+    //menampilkan data struct
+    cout << endl;
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat = " << mhs.alamat << endl;
+
+}
