d7908d783b59199f20e11e37939373f060649243
julianferdiyanto <julianferdiyanto3@gmail.com>
2025-04-15 11:53:32 +0700

    membuat deklarasi global pada program structGetLine

---
+++ structGetLine.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+
+struct mahasiswa
+{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
